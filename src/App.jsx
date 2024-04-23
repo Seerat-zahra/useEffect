d1a1faa77604e7546a9965cc870128d50cbd3551
Seerat-zahra <seeratzahra108@gmail.com>
2024-04-23 01:06:59 -0700

    add project

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React,{useEffect,useState} from 'react';
+import axios from "axios";
+
+function App() {
+
+  const [getdata,setgetdata]=useState(null);
+
+  useEffect(()=>{
+    const getDAta=async()=>{
+      const res=await  axios.get('https://dummyjson.com/products/1');
+      console.log(res.status)
+
+      setgetdata(res.data)
+
+    }
+    getDAta();
+
+
+    
+
+  },[])
+  
+  console.log(getdata)
+  return (
+    <div>
+      react app
+      {
+        getdata && (<>
+          <h1>{getdata.title}</h1>
+      <p>{getdata.description}</p>
+        
+        </>)
+      }
+    
+    </div>
+  )
+}
+
+export default App
